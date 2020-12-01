69a6c405ef2fa28e6ed1d7e3bb521f0cad2148e9
purplecode2020 <purplecode2020@gmail.com>
2020-12-01 20:50:48 +0900

    [FINISH] TodoList

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,28 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { useSetRecoilState } from "recoil";
+import { todosState } from "../recoil/todos";
 
-const TodoItem = () => {
+const TodoItem = ({ data }) => {
+  const setTodos = useSetRecoilState(todosState);
+
+  const toggleTodo = () => {
+    setTodos(todos =>
+      todos.map(todo =>
+        todo.id === data.id ? { ...data, completed: !data.completed } : todo
+      )
+    );
+  };
+
+  const removeTodo = () => {
+    setTodos(todos => todos.filter(todo => todo.id !== data.id));
+  };
+
   return (
-    <Container>
-      <ToggleButton type="checkbox" />
-      <Text>aaaaaaaaaaaaaaaaaaa</Text>
-      <DestroyButton>X</DestroyButton>
+    <Container completed={data.completed}>
+      <ToggleButton type="checkbox" onClick={toggleTodo} />
+      <Text>{data.text}</Text>
+      <DestroyButton onClick={removeTodo}>X</DestroyButton>
     </Container>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
+import { useRecoilValue } from "recoil";
+import { todosState } from "../recoil/todos";
 
 const TodoList = () => {
+  const todos = useRecoilValue(todosState);
+
   return (
     <Container>
       <ListContainer>
-        <TodoItem />
+        {todos.map(todo => (
+          <TodoItem key={todo.id} data={todo} />
+        ))}
       </ListContainer>
     </Container>
   );
